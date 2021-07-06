de6dc70468de4a6878cc08abd6d45041ad3d31a9
DebuggingaHopper <49813790+DebuggingaHopper@users.noreply.github.com>
2021-07-06 13:14:42 -0400

    Add files via upload

---
+++ pyscript_CatBoost_Uproto.py
@@ -0,0 +1,24 @@
+# Parameters for Weka:
+    # num_leaves (default 31)
+    # learning_rate (default 0.03)
+    # n_estimators (I utilized 1000)
+  
+from wekapyscript import ArffToArgs
+import lightgbm
+import catboost
+
+def train(args):
+    x_train = args["X_train"]
+    y_train = args["y_train"]
+    rf = catboost.CatBoostClassifier(num_leaves = args["num_leaves"], learning_rate = args["learning_rate"], n_estimators = args["n_estimators"])
+    rf.fit(x_train, y_train)
+    return rf
+
+
+
+def describe(args, model):
+    return "catboost is running"
+
+def test(args, model):
+    X_test = args["X_test"]
+    return model.predict_proba(X_test).tolist()
