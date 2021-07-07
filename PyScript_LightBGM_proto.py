beeda09589cc2a40ffcb85233699d6af9e7f5a1f
DebuggingaHopper <49813790+DebuggingaHopper@users.noreply.github.com>
2021-07-07 09:15:33 -0400

    Update PyScript_LightBGM_proto.py

--- PyScript_LightBGM_proto.py
+++ PyScript_LightBGM_proto.py
@@ -1,4 +1,4 @@
-# Weka LightGBM
+# Weka LightGBM ProtoType
 # An implementation of LightGBM in Weka, using Weka PyScript
 # For Capitol Technology University's Computer Science Lab
 
@@ -8,6 +8,7 @@
 from wekapyscript import ArffToArgs
 import lightgbm as lgb
 
+# Train the model
 def train(args):
     x_train = args["X_train"]
     y_train = args["y_train"]
@@ -15,11 +16,13 @@ def train(args):
     lgbm = lgbm.fit(x_train, y_train)
     return lgbm
 
-
+# Model description
+# TODO: Add args to return string
 def describe(args, model):
     return "LightGBM is running"
 
-
+# Test the model
+# TODO: Test in Weka
 def test(args, model):
     X_test = args["X_test"]
     return model.predict_proba(X_test).tolist()
