2c79ef1c433368d8f021c9058db31615d6c86ec3
DebuggingaHopper <49813790+DebuggingaHopper@users.noreply.github.com>
2021-07-07 09:14:40 -0400

    Update pyscript_CatBoost_proto.py

--- pyscript_CatBoost_proto.py
+++ pyscript_CatBoost_proto.py
@@ -1,4 +1,4 @@
-# Weka CatBoost
+# Weka CatBoost ProtoType
 # An implementation of CatBoost in Weka, using Weka PyScript
 # For Capitol Technology University's Computer Science Lab
 
@@ -8,6 +8,7 @@
 from wekapyscript import ArffToArgs
 import catboost
 
+# Train the model
 def train(args):
     x_train = args["X_train"]
     y_train = args["y_train"]
@@ -15,11 +16,13 @@ def train(args):
     CB.fit(x_train, y_train)
     return CB
 
-
-
+# Model description
+# TODO: Add args to return string
 def describe(args, model):
     return "catboost is running"
 
+# Test the model
+# TODO: Test in Weka
 def test(args, model):
     X_test = args["X_test"]
     return model.predict_proba(X_test).tolist()
